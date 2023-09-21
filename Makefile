f7fe66c83e67a62ad3ef3bd05dfa27d1ea9e769b
Larouimohammed <m.laroui@esi-sba.dz>
2023-09-21 09:02:04 +0000

    latest

--- Makefile
+++ Makefile
@@ -2,3 +2,7 @@ build:
 	@go build  -o bin/main cmd/main.go
 run: build
 	@ ./bin/main
+build-metriko:
+	@go build  -o bin/main2 metriko-agent/main.go
+run-metriko: build-metriko
+	@ ./bin/main2
---
+++ metriko-server/metriko-server.go
@@ -0,0 +1,80 @@
+package metrikoserver
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"metriko/db"
+	"metriko/hardware"
+	"net"
+	"time"
+
+	"github.com/sirupsen/logrus"
+)
+
+type Message struct {
+	Type         string
+	Cpupayload   hardware.CPU
+	Ifacepayload []hardware.Iface
+}
+
+type Server struct {
+	Addr    string
+	Cpudb   db.CpuMetricStor
+	Ifacedb db.IfaceMetricStor
+}
+
+func NewServer(Cpudb db.CpuMetricStor, Ifacedb db.IfaceMetricStor, addr string) *Server {
+	return &Server{
+		Cpudb:   Cpudb,
+		Ifacedb: Ifacedb,
+		Addr:    addr,
+	}
+}
+
+func (s *Server) Start() {
+	logrus.WithFields(logrus.Fields{"time": time.Now()}).Info("Metriko Server Starting on adress :" + s.Addr)
+	listener, err := net.Listen("tcp", s.Addr)
+	if err != nil {
+		fmt.Println("Error listening:", err)
+		return
+	}
+
+	conn, err := listener.Accept()
+	if err != nil {
+		fmt.Println("Error accepting connection:", err)
+		return
+	}
+	buffer := make([]byte, 1024)
+	for {
+		msg := Message{}
+		msg.Type = "request"
+		data, err := json.Marshal(msg)
+		conn.Write(data)
+		n, err := conn.Read(buffer)
+		if err != nil {
+			fmt.Printf("can't read from connection :%v\n", err)
+			continue
+		}
+		err1 := json.Unmarshal(buffer[:n], &msg)
+		fmt.Println(msg)
+		if err1 != nil {
+			fmt.Printf("can't unmarshal json :%v\n", err)
+			continue
+		}
+
+		_, err = s.Cpudb.Insertcpu(context.Background(), msg.Cpupayload)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = s.Ifacedb.InsertIface(context.Background(), msg.Ifacepayload)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer listener.Close()
+		time.Sleep(1 * time.Second)
+	}
+}
